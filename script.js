e444b90dd8707f8a2d91c5c49309d36ee0eb2794
Seamus Wintheiser MD <38235426+billneftleberg@users.noreply.github.com>
2024-11-13 02:25:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const heroButton = document.querySelector(".hero button");
+    heroButton.addEventListener("click", function() {
+        alert("Welcome to our shop!");
+    });
+});
